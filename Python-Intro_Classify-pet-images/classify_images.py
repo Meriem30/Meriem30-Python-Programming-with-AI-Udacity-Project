8fd10075fceed29a681336aa2e288e5b354f9819
Meriem30 <m.arbaoui@esi-sba.dz>
2024-12-05 12:35:45 +0100

    [UPDATE] complete path image passed to the classifier function

--- Python-Intro_Classify-pet-images/classify_images.py
+++ Python-Intro_Classify-pet-images/classify_images.py
@@ -67,8 +67,9 @@ def classify_images(images_dir, results_dic, model):
     """
     # go through each file in the specified folder
     for file_name in results_dic:
+      full_path = f"{images_dir}/{file_name}"
       # call the classifier to get the label, as in test_classifier.py
-      model_label = classifier(images_dir,model)
+      model_label = classifier(full_path,model)
 
       # transform the returned value of label to match the used format for extracting dog names
       model_label = model_label.strip().lower()
